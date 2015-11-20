47fcaa1b61c2b5618c5c036e48ceb458760b4146
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2015-11-20 13:28:24 -0500

    add grid submission script

---
+++ scripts/submit.sh
@@ -0,0 +1,14 @@
+set -x
+
+for type in number pos1 pos2 pos3
+do
+    python2 Run.py \
+	    --version ${VERSION:-1} \
+	    --type $type \
+	    --process JZ4 \
+	    --submit-dir JZ4 \
+	    --driver grid \
+	    --grid-input group.perf-idtracking:group.perf-idtracking.361024.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ4W.AOD_TIDE.e3668_s2576_s2132_r6765_v5_wSiHits_EXT0 
+    rm -rf JZ4
+	 		
+done
