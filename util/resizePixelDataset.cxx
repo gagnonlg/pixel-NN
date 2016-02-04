db111956c8427513fabe06f1c6dc67287973c1f7
Louis-Guillaume Gagnon <louis.guillaume.gagnon@gmail.com>
2016-02-04 14:59:20 -0500

    resizePixelDataset: remove support for specific cluster type limits
    Was causing a bug where the target variables were becoming garbage

--- util/resizePixelDataset.cxx
+++ util/resizePixelDataset.cxx
@@ -6,14 +6,11 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	size_t n1,c1;
-	size_t n2,c2;
-	size_t n3,c3;
 	size_t n,c;
 	size_t skip = 0;
 	char *treename = "NNinput";
 
-	n1 = c1 = n2 = c2 = n3 = c3 = n = c = 0;
+	n = c = 0;
 
 	for (i = 1; i < argc && argv[i][0] == '-'; i += 2) {
 		if (!argv[i+1]) {
@@ -21,15 +18,6 @@ int main(int argc, char *argv[])
 			return 1;
 		}
 		switch (argv[i][1]) {
-		case '1':
-			n1 = std::atoll(argv[i+1]);
-			break;
-		case '2':
-			n2 = std::atoll(argv[i+1]);
-			break;
-		case '3':
-			n3 = std::atoll(argv[i+1]);
-			break;
 		case 'n':
 			n = std::atoll(argv[i+1]);
 			break;
@@ -46,7 +34,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (argc - i < 2) {
-		std::fprintf(stderr, "usage: %s [-{1,2,3,n} N] out.root in.root in2.root ...\n", argv[0]);
+		std::fprintf(stderr, "usage: %s [-n N] out.root in.root in2.root ...\n", argv[0]);
 		return 1;
 	}
 
@@ -65,61 +53,15 @@ int main(int argc, char *argv[])
 
 	TTree *tree = (TTree*)chain.GetTree()->CloneTree(0);
 
-	float particles1;
-	if (chain.FindBranch("NN_nparticles1"))
-		chain.SetBranchAddress("NN_nparticles1", &particles1);
-	else
-		particles1 = 1;
-
-	float particles2;
-	if (chain.FindBranch("NN_nparticles2"))
-		chain.SetBranchAddress("NN_nparticles2", &particles2);
-	else if (chain.FindBranch("NN_position_id_X_1")) {
-		particles1 = 0;
-		particles2 = 1;
-	} else {
-		particles2 = 0;
-	}
-
-	float particles3;
-	if (chain.FindBranch("NN_nparticles3"))
-		chain.SetBranchAddress("NN_nparticles3", &particles3);
-	else if (chain.FindBranch("NN_position_id_X_2")) {
-		particles1 = 0;
-		particles2 = 0;
-		particles3 = 1;
-	} else {
-		particles3 = 0;
-	}
-
-	for (Long64_t j = skip; j < chain.GetEntries() && (c1 < n1 || c2 < n2 || c3 < n3 || c < n); ++j) {
+	for (Long64_t j = skip; j < chain.GetEntries() && c < n; ++j) {
 		chain.GetEntry(j);
 		if (n > 0) {
 			c++;
 			tree->Fill();
-		} else {
-			if ((c1 < n1) && particles1) {
-				c1++;
-				tree->Fill();
-			} else if ((c2 < n2) && particles2) {
-				c2++;
-				tree->Fill();
-			} else if ((c3 < n3) && particles3) {
-				c3++;
-				tree->Fill();
-			}
 		}
 	}
 
-	if (c1 < n1)
-		std::fprintf(stderr, "WARNING: missing 1 particles clusters\n");
-	if (c2 < n2)
-		std::fprintf(stderr, "WARNING: missing 2 particles clusters\n");
-	if (c3 < n3)
-		std::fprintf(stderr, "WARNING: missing 3 particles clusters\n");
-
 	outfile.Write(0,TObject::kWriteDelete);
-
 
 	return 0;
 }
