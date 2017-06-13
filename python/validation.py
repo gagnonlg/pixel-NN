07ce8fc687148452d244b72f4e9f02fbbf174407
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2017-06-13 15:16:19 -0400

    all residual/pull histograms

--- python/validation.py
+++ python/validation.py
@@ -4,44 +4,87 @@ import ROOT
 ROOT.gROOT.SetBatch(True)
 
 templates = [
-    ('residuals_{n}_{d}', '(Output_positions_{d}[{i}] - Output_true_{d}[{i}])'),
-    ('pull_{n}_{d}', '(Output_positions_{d}[{i}] - Output_true_{d}[{i}])/Output_uncert_{d}[{i}]'),
-    ('corr_residuals_{n}_{d}', '(Output_corr_positions_{d}[{i}] - Output_corr_true_{d}[{i}])'),
-    ('corr_pull_{n}_{d}', '(Output_corr_positions_{d}[{i}] - Output_corr_true_{d}[{i}])/Output_corr_uncert_{d}[{i}]')
+   ('residuals_{n}_{d}', '(Output_positions_{d} - Output_true_{d})'),
+   ('pull_{n}_{d}', '(Output_positions_{d} - Output_true_{d})/Output_uncert_{d}'),
+   ('corr_pull_{n}_{d}', '(Output_corr_positions_{d} - Output_corr_true_{d})/Output_corr_uncert_{d}'),
+   ('residuals_{n}_2D', '(Output_positions_Y - Output_true_Y):(Output_positions_X - Output_true_X)'),
+   ('pull_{n}_2D', '((Output_positions_Y - Output_true_Y)/Output_uncert_Y):((Output_positions_X - Output_true_X)/Output_uncert_X)'),
+   ('corr_pull_{n}_2D', '(Output_corr_positions_Y - Output_corr_true_Y)/Output_corr_uncert_Y:(Output_corr_positions_X - Output_corr_true_X)/Output_corr_uncert_X'),
+   ('residuals_{n}_{d}_2D_eta', '(Output_positions_{d} - Output_true_{d}):globalEta'),
+   ('residuals_{n}_{d}_2D_phi', '(Output_positions_{d} - Output_true_{d}):globalPhi'),
+   ('residuals_{n}_{d}_2D_cluster_size', '(Output_positions_{d} - Output_true_{d}):cluster_size'),
+   ('residuals_{n}_{d}_2D_cluster_size_X', '(Output_positions_{d} - Output_true_{d}):cluster_size_X'),
+   ('residuals_{n}_{d}_2D_cluster_size_Y', '(Output_positions_{d} - Output_true_{d}):cluster_size_Y'),
+   ('pull_{n}_{d}_2D_eta', '(Output_positions_{d} - Output_true_{d})/Output_uncert_{d}:globalEta'),
+   ('pull_{n}_{d}_2D_phi', '(Output_positions_{d} - Output_true_{d})/Output_uncert_{d}:globalPhi'),
+   ('pull_{n}_{d}_2D_cluster_size', '(Output_positions_{d} - Output_true_{d})/Output_uncert_{d}:cluster_size'),
+   ('pull_{n}_{d}_2D_cluster_size_X', '(Output_positions_{d} - Output_true_{d})/Output_uncert_{d}:cluster_size_X'),
+   ('pull_{n}_{d}_2D_cluster_size_Y', '(Output_positions_{d} - Output_true_{d})/Output_uncert_{d}:cluster_size_Y'),
+   ('corr_pull_{n}_{d}_2D_eta', '(Output_corr_positions_{d} - Output_true_{d})/Output_corr_uncert_{d}:globalEta'),
+   ('corr_pull_{n}_{d}_2D_phi', '(Output_corr_positions_{d} - Output_true_{d})/Output_corr_uncert_{d}:globalPhi'),
+   ('corr_pull_{n}_{d}_2D_cluster_size', '(Output_corr_positions_{d} - Output_true_{d})/Output_corr_uncert_{d}:cluster_size'),
+   ('corr_pull_{n}_{d}_2D_cluster_size_X', '(Output_corr_positions_{d} - Output_true_{d})/Output_corr_uncert_{d}:cluster_size_X'),
+   ('corr_pull_{n}_{d}_2D_cluster_size_Y', '(Output_corr_positions_{d} - Output_true_{d})/Output_corr_uncert_{d}:cluster_size_Y'),
 ]
 
 numbers = [1, 2, 3]
 directions = ['X', 'Y']
 layers = [
-    ('all', None),
     ('ibl', '((NN_layer==0)&&(NN_barrelEC==0))'),
     ('barrel', '((NN_layer>0)&&(NN_barrelEC==0))'),
     ('endcap', '(NN_barrelEC!=0)'),
 ]
 
+done = set()
+
 def gen_variables():
+    global done
     it = itertools.product(templates, numbers, directions, layers)
     for (h,v), n, d, (ln,lc) in it:
         name = h.format(n=n,d=d) + '_' + ln
+        if name in done:
+            continue
+        else:
+            done.add(name)
         var = v.format(i=(n-1), d=d)
         cond = '(Output_number_true=={})'.format(n)
         if lc is not None:
             cond += ('&&' + lc)
         yield name, var, cond
 
-def get_range(name):
-    if 'residuals' in name:
-        if 'X' in name:
-            return -0.05, 0.05
-        else:
-            return -0.5, 0.5
+def get_range(var):
+
+    def _range(var):
+        if '_uncert_' in var:
+            return 100, -5, 5
+        if 'positions_X' in var:
+            return 100, -0.05, 0.05
+        if 'positions_Y' in var:
+            return 100, -0.5, 0.5
+        if var == 'globalEta':
+            return 100, -5, 5
+        if var == 'globalPhi':
+            return 100, -4, 4
+        if var == 'cluster_size':
+            return 50, 0, 50
+        if var in ['cluster_size_X', 'cluster_size_Y']:
+            return 8, 0, 8
+
+    fields = var.split(':')
+    if len(fields) == 1:
+        r = _range(fields[0])
+        return '({},{},{})'.format(*r)
     else:
-        return -5, 5
+        r = _range(fields[1]) + _range(fields[0])
+        return '({},{},{},{},{},{})'.format(*r)
 
 def make_hist(tree, name, var, cond):
-    print name, var, cond
-    binmin, binmax = get_range(name)
-    varg = '{}>>{}(1000,{},{})'.format(var,name,binmin,binmax)
+    print name
+
+    if '_2D' in name:
+        varg = '{}>>{}{}'.format(var,name,get_range(var))
+    else:
+        varg = '{}>>{}{}'.format(var,name,get_range(var))
     tree.Draw(varg, cond)
     hist = ROOT.gROOT.Get(name)
     return hist
