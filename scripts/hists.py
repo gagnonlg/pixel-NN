20f73332679f304b497c51b7a3d96de9e9c43afd
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2017-06-20 15:45:33 -0400

    Add scripts to create histograms

---
+++ scripts/hists.py
@@ -0,0 +1,10 @@
+import argparse
+import importlib
+
+args = argparse.ArgumentParser()
+args.add_argument('--input', required=True)
+args.add_argument('--output', required=True)
+args = args.parse_args()
+
+validation = importlib.import_module('.validation', 'pixel-NN')
+validation.make_histograms(args.input, args.output)
