1faefe37284ab42f847a10605d5d6a4f7e5374fe
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2017-07-03 17:23:52 -0400

    Update figures script

--- scripts/figures.py
+++ scripts/figures.py
@@ -1,6 +1,7 @@
 """ Program to produce the pixel clustering NN performance plots """
 import argparse
 import collections
+import itertools
 import logging
 import os
 import sys
@@ -12,6 +13,7 @@ LOG = logging.getLogger(os.path.basename(__file__))
 NPARTICLES = [1, 2, 3]
 LAYERS = ['ibl', 'barrel', 'endcap']
 DIRECTIONS = ['X', 'Y']
+VARIABLES = ['pull', 'corr_pull', 'residuals']
 
 
 def _get_args():
@@ -42,64 +44,157 @@ def _init_root():
     ROOT.SetAtlasStyle()
 
 
-def _plot_1d_residuals(hsdict, preliminary=False):
+def _fit(thist):
+    sig = thist.GetStdDev()
+    thist.Fit('gaus', '0', '', -3 * sig, 3 * sig)
+    fit = thist.GetFunction('gaus')
+    return fit.GetParameter('Mean'), fit.GetParameter('Sigma')
 
-    npart = 1
-    direc = 'X'
-    name = 'residuals_{}_{}'.format(npart, direc)
 
-    cnv = ROOT.TCanvas('canvas_' + name, '', 0, 0, 800, 600)
-    stk = ROOT.THStack()
-    leg = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
-    leg.SetBorderSize(0)
-    width = 0
-    for lyr in LAYERS:
-        h_name = '{}_{}'.format(name, lyr)
-        hist = hsdict[h_name]
-        if lyr == 'ibl':
-            hist.SetLineColor(ROOT.kRed)
-            hist.SetMarkerColor(ROOT.kRed)
-            hist.SetMarkerStyle(21)  # square
-            leg.AddEntry(hist, 'IBL clusters', 'LP')
-        elif lyr == 'barrel':
-            hist.SetLineColor(ROOT.kBlack)
-            hist.SetMarkerColor(ROOT.kBlack)
-            hist.SetMarkerStyle(8)  # circle
-            leg.AddEntry(hist, 'Barrel clusters', 'LP')
-        else:  # endcap
-            hist.SetLineColor(ROOT.kBlue)
-            hist.SetMarkerColor(ROOT.kBlue)
-            hist.SetMarkerStyle(23)  # inverted triangle
-            leg.AddEntry(hist, 'Endcap clusters', 'LP')
+def _fwhm(thist):
+    bin1 = thist.FindFirstBinAbove(thist.GetMaximum() * 0.5)
+    bin2 = thist.FindLastBinAbove(thist.GetMaximum() * 0.5)
+    return thist.GetBinCenter(bin2) - thist.GetBinCenter(bin1)
+
+
+class Plot(object):
+    """ Encapsulate plotting code """
+    # pylint: disable=too-many-instance-attributes
+    def __init__(self, var, name, nparticle, direction):
+        self.var = var.replace('corr_', '')
+        self.nparticle = nparticle
+        self.direction = direction
+        self.canvas = ROOT.TCanvas('canvas_' + name, '', 0, 0, 800, 600)
+        self.stack = ROOT.THStack()
+        self.legend = ROOT.TLegend(0.6, 0.6, 0.9, 0.9)
+        self.legend.SetBorderSize(0)
+        self.hists = []
+        self.width = None
+        self.unit = None
+        self.name = name
+
+        self.scaley = 1.3
+        if nparticle > 1:
+            self.scaley = 1.7
+
+        if var == 'pull':
+            self.rangex = 5.0
+        else:
+            if direction == 'X':
+                if nparticle == 1:
+                    self.rangex = 0.04
+                else:
+                    self.rangex = 0.05
+            else:
+                self.rangex = 0.4
+
+    def add(self, hist, layer, color, marker):
+        """ Add hist to plot """
+        self.hists.append(hist)
+        hist.SetLineColor(color)
+        hist.SetMarkerColor(color)
+        hist.SetMarkerStyle(marker)
+
+        if self.var == 'pull':
+            mean, sigma = _fit(hist)
+        else:
+            mean = hist.GetMean()
+            sigma = _fwhm(hist)
+
+        self.unit = '' if self.var == 'pull' else 'mm'
+        self.legend.AddEntry(
+            hist,
+            '#splitline{%s clusters}'
+            '{#mu = %.3f %s, %s = %.3f %s}' % (
+                layer,
+                mean,
+                self.unit,
+                "fwhm" if self.var == 'residuals' else "#sigma",
+                sigma,
+                self.unit
+            ),
+            'LP'
+        )
+
         hist.Rebin(2)
-        width = hist.GetBinWidth(1)
-        stk.Add(hist)
-    stk.SetTitle(
-        ';Truth hit residual [mm];Cluster density / {} mm'.format(width)
-    )
-    stk.Draw('hist nostack')
-    stk.Draw('p same nostack')
-    leg.Draw()
-    stk.SetMaximum(stk.GetMaximum('nostack') * 1.3)
-    stk.GetXaxis().SetRangeUser(-0.04, 0.04)
-    ROOT.ATLAS_LABEL(0.2, 0.88)
-    txt = ROOT.TLatex()
-    txt.SetNDC()
-    txt.DrawText(
-        0.33,
-        0.88,
-        'Simulation {}'.format('Preliminary' if preliminary else 'Internal')
-    )
-    txt.SetTextSize(txt.GetTextSize() * 0.65)
-    txt.DrawLatex(0.2, 0.78, 'PYTHIA8 dijet, 1.8 < p_{T}^{jet} < 2.5 TeV')
-    txt.DrawText(
-        0.2,
-        0.73,
-        '{} particle{} clusters'.format(npart, 's' if npart > 1 else '')
-    )
-    txt.DrawText(0.2, 0.68, 'local {} direction'.format(direc))
-    cnv.SaveAs(name + '.pdf')
+        self.width = hist.GetBinWidth(1)
+        self.stack.Add(hist)
 
+    def draw(self, preliminary=False):
+        """ Draw the plot """
+        bunit = '[{}]'.format(self.unit) if self.var != 'pull' else ''
+        self.stack.SetTitle(
+            ';Truth hit {} {};Cluster density / {} {}'.format(
+                self.var,
+                bunit,
+                self.width,
+                self.unit
+            )
+        )
+        self.stack.Draw('hist nostack')
+        self.stack.Draw('p same nostack')
+        self.stack.SetMaximum(self.stack.GetMaximum('nostack') * self.scaley)
+        self.stack.GetXaxis().SetRangeUser(-self.rangex, self.rangex)
+
+        self.legend.Draw()
+
+        ROOT.ATLAS_LABEL(0.2, 0.88)
+        txt = ROOT.TLatex()
+        txt.SetNDC()
+        txt.DrawText(
+            0.33,
+            0.88,
+            'Simulation {}'.format(
+                'Preliminary' if preliminary else 'Internal'
+            )
+        )
+        txt.SetTextSize(txt.GetTextSize() * 0.65)
+        txt.DrawLatex(0.2, 0.78, 'PYTHIA8 dijet, 1.8 < p_{T}^{jet} < 2.5 TeV')
+        txt.DrawText(
+            0.2,
+            0.73,
+            '{}-particle clusters'.format(self.nparticle)
+        )
+        txt.DrawText(0.2, 0.68, 'local {} direction'.format(self.direction))
+        self.canvas.SaveAs(self.name + '.pdf')
+
+
+def _plot_1d_hists(hsdict, preliminary=False):
+
+    prod = itertools.product(VARIABLES, NPARTICLES, DIRECTIONS)
+    for var, npart, direc in prod:
+
+        name = '_'.join([var, str(npart), direc])
+        LOG.info(name)
+
+        plot = Plot(var, name, npart, direc)
+
+        for lyr in LAYERS:
+            h_name = '{}_{}'.format(name, lyr)
+            hist = hsdict[h_name]
+            if lyr == 'ibl':
+                plot.add(
+                    hist=hist,
+                    layer='IBL',
+                    color=ROOT.kRed,
+                    marker=21  # square
+                )
+            elif lyr == 'barrel':
+                plot.add(
+                    hist=hist,
+                    layer='Barrel',
+                    color=ROOT.kBlack,
+                    marker=8  # circle
+                )
+            else:  # endcap
+                plot.add(
+                    hist=hist,
+                    layer='Endcap',
+                    color=ROOT.kBlue,
+                    marker=23  # inverted triangle
+                )
+
+        plot.draw(preliminary)
 
 ########################################################################
 # Main
@@ -114,7 +209,7 @@ def _main():
     hists = _get_histograms(args.input)
     for histname, thist in hists.iteritems():
         LOG.debug('%s: %s', histname, str(thist))
-    _plot_1d_residuals(hists, preliminary=args.preliminary)
+    _plot_1d_hists(hists, preliminary=args.preliminary)
 
     return 0
 
