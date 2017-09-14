609664ad6b3714f4759a97c893d8042b6a4cbc62
Louis-Guillaume Gagnon <louis.guillaume.gagnon@cern.ch>
2017-09-14 05:47:30 -0400

    Add launch script to create validation histograms

---
+++ scripts/do_validation.py
@@ -0,0 +1,13 @@
+import argparse
+import importlib
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+args = argparse.ArgumentParser()
+args.add_argument('input')
+args.add_argument('output')
+args = args.parse_args()
+
+validation = importlib.import_module('.validation', 'pixel-NN')
+validation.make_histograms(input_paths, output_path)
